a9fa00544ebdf30d4e39d1e5f973d26af5c17cce
Jason <pito200decimals@gmail.com>
2015-04-18 19:29:09 -0700

    Test commit

--- html/header.php
+++ html/header.php
@@ -1,6 +1,9 @@
 <?php
 // Standard header that includes all the important files. Will reside in the site root.
 
+// Test conflicting edit!
+// This is a useless comment
+
 // Find the site root directory.
 $folder_level = "";
 while (!file_exists($folder_level.__FILE__)) {
