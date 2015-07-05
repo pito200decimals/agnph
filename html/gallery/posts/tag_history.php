f7385aeef798be76031f35ec8e3f5c6506610246
Jason <pito200decimals@gmail.com>
2015-07-04 17:13:31 -0700

    Added properties to tag history

--- html/gallery/posts/tag_history.php
+++ html/gallery/posts/tag_history.php
@@ -35,6 +35,10 @@ if (sizeof($tag_history_items) > 0) {
         $tags = GetTagsById(GALLERY_TAG_TABLE, $all_tag_ids);
         if ($tags == null) RenderErrorPage("Post not found");
     }
+    $rating = "";
+    $parent = "none";
+    $source = "";
+    $tag_history_items = array_reverse($tag_history_items);  // Need to process in reverse order.
     foreach ($tag_history_items as &$item) {
         $tag_changes = "";
         $adds = array_map(function($tag_id) use ($tags) {
@@ -46,6 +50,32 @@ if (sizeof($tag_history_items) > 0) {
             return "<span class='nscore'>-</span><span class='$typeclass'>".$tags[$tag_id]['Name']."</span>";
         }, array_filter(explode(",", $item['TagsRemoved']), "mb_strlen"));
         $edits = array_merge($adds, $removes);
+        // Add in rating/source/parent changes.
+        $propsChanged = $item['PropertiesChanged'];
+        if (mb_strlen($propsChanged) > 0) {
+            $props = explode(" ", $propsChanged);
+            foreach ($props as $prop) {
+                if (startsWith($prop, "rating:")) {
+                    $r = mb_substr($prop, 7);
+                    if ($r != $rating) {
+                        $edits[] = "<span class='ptypetag'>$prop</span>";
+                        $rating = $r;
+                    }
+                } else if (startsWith($prop, "parent:")) {
+                    $p = mb_substr($prop, 7);
+                    if ($p != $parent) {
+                        $edits[] = "<span class='ptypetag'>$prop</span>";
+                        $parent = $p;
+                    }
+                } else if (startsWith($prop, "source:")) {
+                    $s = mb_substr($prop, 7);
+                    if ($s != $source) {
+                        $edits[] = "<span class='ptypetag'>$prop</span>";
+                        $source = $s;
+                    }
+                }
+            }
+        }
         sort($edits);
         $edits = array_map(function($html) {
             return "<span class='tag-edit'>$html</span>";
@@ -55,6 +85,7 @@ if (sizeof($tag_history_items) > 0) {
         $item['date'] = FormatDate($item['Timestamp']);
         LoadSingleTableEntry(array(USER_TABLE), "UserId", $item['UserId'], $item['user']);
     }
+    $tag_history_items = array_reverse($tag_history_items);  // Undo reverse order.
 
     // TODO: Also add metadata changes like rating, etc.
 }
