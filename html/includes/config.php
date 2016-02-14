1b220258287fc10ad2f711440f58bad6ee1ebb57
Jason <pito200decimals@gmail.com>
2016-02-13 20:13:29 -0800

    Added flag to easily change debug php logging settings

--- html/includes/config.php
+++ html/includes/config.php
@@ -1,17 +1,20 @@
 <?php
 // General site-authentication code, used to define access to server db's and such.
 
-// Debug settings.
-//ini_set("display_errors", "On");
-//ini_set("display_startup_errors", "On");
-//ini_set("error_reporting", E_ALL);
-// Non-debug settings
-
-ini_set("display_errors", "Off");
-ini_set("display_startup_errors", "Off");
-ini_set("error_reporting", 0);
-set_time_limit(120);
-ini_set('memory_limit', '100M');
+$use_debug_php_settings = false;
+if ($use_debug_php_settings) {
+    // Debug settings.
+    ini_set("display_errors", "On");
+    ini_set("display_startup_errors", "On");
+    ini_set("error_reporting", E_ALL);
+} else {
+    // Non-debug settings
+    ini_set("display_errors", "Off");
+    ini_set("display_startup_errors", "Off");
+    ini_set("error_reporting", 0);
+    set_time_limit(120);
+    ini_set('memory_limit', '100M');
+}
 
 
 // MySQL DB settings.
