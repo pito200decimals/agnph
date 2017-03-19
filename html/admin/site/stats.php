57114f8f14dc951ef37ee6ef1f90c3396a536d6b
Jason <pito200decimals@gmail.com>
2017-03-18 18:32:47 -0700

    Fix missing column header on admin stats page.

--- html/admin/site/stats.php
+++ html/admin/site/stats.php
@@ -30,6 +30,7 @@ if (isset($_GET['duration'])) {
     }
 }
 $column = "PageUrl";
+$vars['column_type'] = "Page URL";
 if (isset($_GET['type'])) {
     if ($_GET['type'] == "page") {
         $column = "PageUrl";
