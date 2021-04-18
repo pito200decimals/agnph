50a3d4cf175a216339595e2d1a806c093224afee
Jason <pito200decimals@gmail.com>
2021-04-17 17:04:55 -0700

    Fix typo and getStream avatar URL.

--- html/oekaki/site/getStreams.php
+++ html/oekaki/site/getStreams.php
@@ -7,7 +7,7 @@ include_once(SITE_ROOT."ajax_header.php");
 include_once(SITE_ROOT."includes/util/core.php");
 include_once(SITE_ROOT."includes/util/html_funcs.php");
 include_once(SITE_ROOT."gallery/includes/functions.php");
-include_once(SITE_ROOT."oekaki/site/includes.functions.php");
+include_once(SITE_ROOT."oekaki/site/includes/functions.php");
 
 $streams = GetActiveLivestreams();
 $result = array();
@@ -15,7 +15,7 @@ foreach ($streams as $stream) {
     $result[] = array(
         'name' => $stream['DisplayName'],
         'url' => "https://agn.ph/oekaki/draw/#live".$stream['UserId'],
-        'avatar' => $stream['avatarURL'],
+        'avatar' => "https://agn.ph".$stream['avatarURL'],
     );
 }
 
