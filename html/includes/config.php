141f5ba48e8750d3fefedb4fd8ec056b24e3f93f
zwall-bp <the@zwall.dev>
2025-08-26 14:50:00 -0700

    Enabled SQL logging in debug

--- html/includes/config.php
+++ html/includes/config.php
@@ -9,6 +9,7 @@ if (USE_DEBUG_PHP_SETTINGS) {
     ini_set("display_errors", "On");
     ini_set("display_startup_errors", "On");
     ini_set("error_reporting", E_ALL);
+    mysqli_report(MYSQLI_REPORT_ERROR); // Log SQL errors, return false
 } else {
     // Non-debug settings
     ini_set("display_errors", "Off");
@@ -16,6 +17,7 @@ if (USE_DEBUG_PHP_SETTINGS) {
     ini_set("error_reporting", E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
     set_time_limit(120);
     ini_set('memory_limit', '600M');
+    mysqli_report(MYSQLI_REPORT_OFF); // Suppress SQL errors, return false instead
 }
 
 
@@ -27,7 +29,5 @@ $dbname = "agnph";
 define("GALLERY_CRYPT_SALT", "");  // Don't show this in github :)
 define("OEKAKI_CRYPT_SALT", "");  // Don't show this in github :)
 define("IRC_MIRROR_POST_SECRET_KEY", "");  // Don't show this in github :)
-
-mysqli_report(MYSQLI_REPORT_OFF); // Suppress SQL errors, return false instead
 
 ?>
